bbc4366917bc70dd14ecbe1d6ca04ee8e756d7be
Tuomas Tuomitie <tuomas@tuomitie.com>
2018-02-08 22:34:08 +0200

    Week 3.13

--- index.js
+++ index.js
@@ -15,6 +15,8 @@ app.use(
 )
 
 const mongoose = require('mongoose')
+const url = 'mongodb://kokopino:verkonpaino@ds227858.mlab.com:27858/puhuttelu'
+mongoose.connect(url)
 
 const Person = mongoose.model('Person', {
     name: String,
