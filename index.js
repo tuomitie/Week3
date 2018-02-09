b049505eb11b2694abf0547e2b2dab727a915eb3
Tuomas Tuomitie <tuomas@tuomitie.com>
2018-02-09 20:32:54 +0200

    Week 3.20

--- index.js
+++ index.js
@@ -8,14 +8,7 @@ const Person = require('./models/person')
 app.use(express.static('build'))
 app.use(bodyParser.json())
 app.use(cors())
-
 app.use(morgan('tiny'))
-
-app.get('/info', (req, res) => {
-    const lkm = persons.length
-    const aika = new Date()
-    res.send(`<p>puhelinluettelossa on ${lkm} henkilön tiedot</p><p>${aika}</p>`)
-})
 
 const formatPerson = (person) => {
     return {
@@ -25,7 +18,7 @@ const formatPerson = (person) => {
     }
 }
 
-app.get('/api/persons', (request, response) => {
+app.get('/api/people', (request, response) => {
     Person
         .find({})
         .then(persons => {
@@ -33,63 +26,104 @@ app.get('/api/persons', (request, response) => {
         })
 })
 
-app.get('/api/persons/:id', (request, response) => {
-    const id = Number(request.params.id)
-    const person = persons.find(person => person.id === id)
-
-if ( person ) {
-    response.json(person)
-} else {
-    response.status(404).end()
-}
+app.get('/api/people/:id', (request, response) => {
+    Person
+        .findById(request.params.id)
+        .then(person => {
+            if (person) {
+                response.json(formatPerson(person))
+            } else {
+                response.status(404).end()
+            }
+        })
+        .catch(error => {
+            console.log(error)
+            response.status(400).send({error: 'malformatted id'})
+        })
 })
 
-const generateId = () => {
-    const maxId = persons.length > 0 ? persons.map(n => n.id).sort().reverse()[0] : 1
-    return maxId + 1
-}
+app.post('/api/people', (request, response) => {
+    const body = request.body
 
-function arvoTunnus() {
-    min = Math.ceil(10000);
-    max = Math.floor(1);
-    return Math.floor(Math.random() * (max - min)) + min; //The maximum is exclusive and the minimum is inclusive
-}
+    if (body.name === undefined) {
+        return response.status(400).json({error: 'name missing'})
+    }
+    if (body.number === undefined) {
+        return response.status(400).json({error: 'number missing'})
+    }
+    /*    if (tarkistaLoytyyko(body.name)) {
+            return response.status(400).json({error: 'person is already listed'})
+    }*/
 
+    const person = new Person({
+        name: body.name,
+        number: body.number
+    })
+
+    person
+        .save()
+        .then(savedPerson => {
+            response.json(formatPerson(savedPerson))
+        })
+        .catch(error => {
+            console.log(error)
+            response.status(400).send({error: 'malformatted id'})
+        })
+})
+
+app.put('/api/people/:id', (request, response) => {
+    const body = request.body
+    const person = {
+        name: body.name,
+        number: body.number
+    }
+
+    Person
+        .findByIdAndUpdate(request.params.id, person, {new: true})
+        .then(updatedPerson => {
+            response.json(formatPerson(updatedPerson))
+        })
+        .catch(error => {           // Not effective in catching unhandled promise rejection :(
+            console.log(error)
+            response.status(400).send({error: 'Duplicate name'})
+        })
+})
+
+app.delete('/api/people/:id', (request, response) => {
+    Person
+        .findByIdAndRemove(request.params.id)
+        .then(result => {
+            response.status(204).end()
+        })
+        .catch(error => {
+            response.status(400).send({error: 'malformatted id'})
+        })
+})
+
+
+app.get('/info', (req, res) => {
+    Person
+        .find({}).count()
+        .then(result => {
+            const aika = new Date()
+            res.send(`<p>puhelinluettelossa on ${result} henkilön tiedot</p><p>${aika}</p>`)
+        }).catch(error => {
+        console.log(error)
+    })
+})
+
+/*
 function tarkistaLoytyyko(nimi) {
     const person = persons.find(person => person.name === nimi)
     return person
 }
+*/
 
-app.post('/api/persons', (request, response) => {
-    const body = request.body
-
-    if (body.name === undefined) {
-    return response.status(400).json({error: 'name missing'})
-}
-    if (body.number === undefined) {
-    return response.status(400).json({error: 'number missing'})
-}
-    if (tarkistaLoytyyko(body.name)) {
-    return response.status(400).json({error: 'person is already listed'})
-    }
-
-const person = {
-    name: body.name,
-    number: body.number,
-    id: Number(arvoTunnus())
-}
-
-persons = persons.concat(person)
-
-response.json(person)
-})
-
-app.delete('/api/persons/:id', (request, response) => {
-    const id = Number(request.params.id)
-    persons = persons.filter(person => person.id !== id)
-
-response.status(204).end()
-})
+/*function arvoTunnus() {       // Ei enää käytössä tietokannan huolehtiessa id:istä
+    min = Math.ceil(10000);
+    max = Math.floor(1);
+    return Math.floor(Math.random() * (max - min)) + min; //The maximum is exclusive and the minimum is inclusive
+}*/
 
 const PORT = process.env.PORT || 3001
 app.listen(PORT, () => {
--- models/person.js
+++ models/person.js
@@ -1,6 +1,6 @@
 const mongoose = require('mongoose')
 
-const url = 'mongodb://kokopino:verkonpaino@ds227858.mlab.com:27858/puhuttelu'
+const url = process.env.MONGODB_URI
 
 mongoose.connect(url)
 
